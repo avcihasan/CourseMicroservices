f9ac21603f4ec663fb550bbc9003f81465518de6
Hasan Avcı <hsnavci7@gmail.com>
2023-06-25 23:42:28 +0300

    FakePayment.API

---
+++ Services/FakePayment/Course.Services.FakePayment/Controllers/FakePaymnetsController.cs
@@ -0,0 +1,17 @@
+﻿using Course.Shared.CustomBaseController;
+using Course.Shared.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Course.Services.FakePayment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymnetsController : CustomBaseController
+    {
+        [HttpPost]
+        public IActionResult Payment()
+            => CreateActionResult(ResponseDto<NoContentDto>.Success(HttpStatusCode.OK));
+    }
+}
---
+++ Services/FakePayment/Course.Services.FakePayment/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+builder.Services.AddControllers(x => x.Filters.Add(new AuthorizeFilter(new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build())));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServerUrl"];
+    options.Audience = "resource_basket";
+    options.RequireHttpsMetadata = false;
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
